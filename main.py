38e857ada5b202adc094acfcaef8da34459e7218
Stan <stan.janssen56@gmail.com>
2023-02-25 10:11:25 -0500

    init

---
+++ main.py
@@ -0,0 +1,157 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Firefox
+# from selenium.webdriver.firefox.service import Service
+# from selenium.webdriver.firefox.options import Options
+
+# Chrome
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+import os
+import time
+from PIL import Image
+from io import BytesIO
+from pathlib import Path
+
+import modules.foliera
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def update_html(element_name, element_data):
+    element = driver.find_element(By.ID, element_name)
+    driver.execute_script(
+        f"arguments[0].innerText = '{element_data}'", element)
+
+def hide_html(element_name):
+    element = driver.find_element(By.ID, element_name)
+    driver.execute_script(f"arguments[0].style.visibility = 'hidden'", element)
+
+def hide_style():
+    element = driver.find_element(By.CLASS_NAME, "section1")
+    driver.execute_script(f"arguments[0].style.display = 'none'", element)
+
+    element = driver.find_element(By.CLASS_NAME, "section2")
+    driver.execute_script(f"arguments[0].style.gridColumn = '1/3'", element)
+
+    element = driver.find_element(By.CLASS_NAME, "section2>div")
+    driver.execute_script(f"arguments[0].style.paddingLeft = '160px'", element)
+
+    element = driver.find_element(By.CLASS_NAME, "section3")
+    driver.execute_script(f"arguments[0].style.gridColumn = '1/3'", element)
+
+def render_label(company,product,size,color,pack_size,upc,price,quantity,code,pot_cover, tag1="", tag2="", tag3="", tag4="", tag5=""):
+    update_html("tag1", tag1)
+    # update_html("tag2", tag2)
+    update_html("tag3", tag3)
+    # update_html("tag4", tag4)
+    update_html("tag5", tag5)
+
+    if company:
+        update_html("company", company)
+    else:
+        hide_style()
+
+    update_html("product", f"{pack_size}x{size}\" {product}")
+    update_html("code", code)
+    update_html("color", color)
+    if pot_cover:
+        update_html("tag2", "Pot Cover")
+    else:
+        update_html("tag2", "")
+    if price:
+        update_html("tag4", price)
+    elif upc:
+        update_html("tag4", "UPC")
+    else:
+        update_html("tag4", "")
+
+    take_screenshot(driver.get_screenshot_as_png(), quantity)
+
+def take_screenshot(image, count):
+    # driver.save_screenshot('test.png')
+    
+    # Converting PNG from RGBA to RGB so it can go into a PDF file
+    pnga = Image.open(BytesIO(image))
+    pnga.load()
+    png = Image.new("RGB", pnga.size, (255, 255, 255))
+    png.paste(pnga, mask=pnga.split()[3])
+    for c in range(0, int(count)):
+        pnglist.append(png)
+
+filepath = input("Drag the label file into this window and press enter:\n")
+filepath = filepath.strip("'")
+pnglist = []
+current_dir = os.getcwd()
+
+start = time.time()
+
+print("starting webdriver...")
+options = Options()
+options.headless = True
+# if options.headless:
+#     options.add_argument("--height=885")
+# else:
+#     options.add_argument("--height=857")
+if options.headless:
+    options.add_argument("--window-size=1200,800")
+else:
+    options.add_argument("--window-size=1200,924")
+# options.add_argument("--width=1200")
+# service = Service(executable_path=f'{current_dir}/geckodriver', log_path=os.devnull)
+# driver = webdriver.Firefox(options=options, service=service)
+service = Service(executable_path=f'{current_dir}/chromedriver', log_path=os.devnull)
+driver = webdriver.Chrome(options=options, service=service)
+driver.get(f'file:///{current_dir}/web/index.html')
+
+labelError = False
+
+for label in modules.foliera.parse(filepath):
+    print(f"rendering label: {label}")
+    try:
+        render_label(company=label.get('company', ''),
+                    product=label.get('product', ''),
+                    size=label.get('size', ''),
+                    color=label.get('color', ''),
+                    pack_size=label.get('pack_size', ''),
+                    upc=label.get('upc', ''),
+                    price=label.get('price', ''),
+                    quantity=label.get('quantity', ''),
+                    code=label.get('code', ''),
+                    pot_cover=label.get('pot_cover', ''),
+                    tag1=label.get('tag1', ''),
+                    tag2=label.get('tag2', ''),
+                    tag3=label.get('tag3', ''),
+                    tag4=label.get('tag4', ''),
+                    tag5=label.get('tag5', ''),)
+    except:
+        labelError = True
+        print(f"{bcolors.BOLD}{bcolors.FAIL}\n{label}\nThis label did NOT render correctly! You MUST print it manually!\n{bcolors.ENDC}")
+
+driver.close()
+
+end = time.time()
+
+timer = (end-start) * 10**3/1000
+print(f"\ncreated {len(pnglist)} labels in {round(timer,2)} seconds ({round(len(pnglist)/timer,2)} l/s)\n")
+
+print("saving to pdf...")
+filename = filepath.rsplit('/', 1)[-1].split(".")[0]
+save_path = Path(current_dir) / "_labels" / filename
+pnglist[0].save(f"{save_path}-LABELS.pdf", "PDF" ,resolution=100.0, save_all=True, append_images=pnglist[1:])
+print(f"\nfile saved to {save_path}-LABELS.pdf")
+
+if labelError:
+    print(f"{bcolors.BOLD}{bcolors.FAIL}\nONE OR MORE LABELS DID NOT RENDER CORRECTLY! YOU MUST PRINT IT MANUALLY!{bcolors.ENDC}")
+print(f"{bcolors.WARNING}\n(!) VERIFY LABELS (!)\n{bcolors.ENDC}")
---
+++ modules/foliera.py
@@ -0,0 +1,106 @@
+from PyPDF2 import PdfReader
+import re
+
+def parse(filename):
+    reader = PdfReader(filename)
+    total_pages = reader.numPages
+    dict = []
+
+    for page_num in range(0, total_pages):
+        page = reader.pages[page_num]
+        extracted_text = page.extract_text()
+        temp = extracted_text.split("Sent To:")[0]
+        # print(temp)
+        # a = re.split("[0-9][0-9][0-9][0-9][0-9][0-9]", temp)
+        a = re.split("(?<!\d)\d{6}(?!\d)", temp)
+        # print(a)
+        # a = re.split(r"\D(\d{6})\D", " "+temp+" ")
+        # print(a)
+        a.pop(0)
+        # print(a)
+        for n, b in enumerate(a):
+            # print("B:",repr(b))
+            # print(repr(b))
+            c = b.split("This Product Total")
+            # print("C:",c)
+            # d = c[0].splitlines()
+            d = c[0].splitlines()
+            d.pop(0)
+            print("D:",d)
+            if not d:
+                # If "d" is an empty list, skip it.
+                break
+
+            e = {}
+            e["product"] = d[0].split()[0].capitalize()
+            try:
+                e["size"] = re.search("[0-9]\"",d[0]).group().strip("\"")
+                # Search for pot sizes ranging from 0-9 with a " after it
+            except:
+                pass
+
+            # Grab the last columns content
+            e["code"] = d[len(d)-1]
+
+            try:
+                # Remove useless information from the color, and fix colors
+                e["color"] = d[0].split("\" ")[1].replace("w/Mylar","")
+                e["color"] = e["color"].replace("Asst","Assorted")
+                e["color"] = e["color"].replace("Flamingo","")
+            except:
+                pass
+
+            if "w/Mylar" in d[0]:
+                e["pot_cover"] = True
+
+            # If the column contains a * then get quantity from the next column over
+            if "*"in d[len(d)-2]:
+                e["quantity"] = d[len(d)-3]
+            else:
+                e["quantity"] = d[len(d)-2]
+
+            for i,j in enumerate(d):
+                if "BX" in j:
+                    e["pack_size"] = d[i].strip("BX")
+                    e["tag5"] = "Box"
+                elif "TR" in j:
+                    e["pack_size"] = d[i].strip("TR")
+                    e["tag5"] = "Tray"
+
+                if "$" in j:
+                    e["price"] = d[i].strip("$")
+
+                # Search for UPC code in 0-00000-00000-0 format
+                if re.search("[0-9]-[0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][0-9]-[0-9]", j):
+                    e["upc"] = d[i]
+                # Search for UPC code in 00000000 format (IKEA)
+                elif re.search("[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]", j):
+                    e["upc"] = d[i]
+
+            #SPECIAL CASES
+            if "tripod" in d[0].lower():
+                e["product"] = "Succulent Tripod Planter"
+                del e["color"]
+
+            if "heart shaped" in d[0].lower():
+                e["product"] = "Heart Shaped Planter"
+                del e["color"]
+
+            if "glass cyl" in d[0].lower():
+                e["product"] = "Anthurium in Glass"
+                e["size"] =  "4"
+
+            if "sample" in d[0].lower():
+                e["color"] = "Sample"
+                for i,j in enumerate(d):
+                    if "POTS" in j:
+                        e["pack_size"] = d[i-1]
+                        e["quantity"] = "1"
+
+            dict.append(e)
+    return(dict)
+
+if __name__ == "__main__":
+   print(parse("PurchaseOrder-WESGRE-01-Dec-22.pdf"))
+    # print(parse("PurchaseOrder-WESGRE-02-Feb-23.pdf"))
+    # print(parse("PurchaseOrder-WESGRE-12-Dec-22-IKEA.pdf"))
