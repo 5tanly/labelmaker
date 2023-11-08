11b412a273f466bd25cf1fe3b402720584b3326e
Stan J <31938727+5tanly@users.noreply.github.com>
2023-11-07 21:26:01 -0500

    Update main.py
    add multiple renderers

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-# Firefox
-# from selenium.webdriver.firefox.service import Service
-# from selenium.webdriver.firefox.options import Options
-
 # Chrome
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -16,6 +12,7 @@ from io import BytesIO
 from pathlib import Path
 
 import modules.foliera
+import modules.trillium
 
 class bcolors:
     HEADER = '\033[95m'
@@ -27,7 +24,6 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
-
 
 def update_html(element_name, element_data):
     element = driver.find_element(By.ID, element_name)
@@ -51,11 +47,18 @@ def hide_style():
     element = driver.find_element(By.CLASS_NAME, "section3")
     driver.execute_script(f"arguments[0].style.gridColumn = '1/3'", element)
 
-def render_label(company,product,size,color,pack_size,upc,price,quantity,code,pot_cover, tag1="", tag2="", tag3="", tag4="", tag5=""):
+def reset_label():
+    #Resets the label so the font is sized correctly
+    list = ["company","product","code","color","tag1","tag2","tag3","tag4", "tag5"]
+    for l in list:
+        update_html(l, "@@@")
+
+def render_label(company,product,size,color,pack_size,upc,price,quantity,code,pot_cover,tag1="",tag2="",tag3="",tag4="",tag5=""):
+    reset_label()
     update_html("tag1", tag1)
-    # update_html("tag2", tag2)
+    update_html("tag2", tag2)
     update_html("tag3", tag3)
-    # update_html("tag4", tag4)
+    update_html("tag4", tag4)
     update_html("tag5", tag5)
 
     if company:
@@ -80,8 +83,6 @@ def render_label(company,product,size,color,pack_size,upc,price,quantity,code,po
     take_screenshot(driver.get_screenshot_as_png(), quantity)
 
 def take_screenshot(image, count):
-    # driver.save_screenshot('test.png')
-    
     # Converting PNG from RGBA to RGB so it can go into a PDF file
     pnga = Image.open(BytesIO(image))
     pnga.load()
@@ -89,7 +90,9 @@ def take_screenshot(image, count):
     png.paste(pnga, mask=pnga.split()[3])
     for c in range(0, int(count)):
         pnglist.append(png)
-
+print("foliera - f")    
+print("trillium - t")     
+company = input("^use the options above\nEnter company:\n")
 filepath = input("Drag the label file into this window and press enter:\n")
 filepath = filepath.strip("'")
 pnglist = []
@@ -100,24 +103,22 @@ start = time.time()
 print("starting webdriver...")
 options = Options()
 options.headless = True
-# if options.headless:
-#     options.add_argument("--height=885")
-# else:
-#     options.add_argument("--height=857")
 if options.headless:
     options.add_argument("--window-size=1200,800")
 else:
     options.add_argument("--window-size=1200,924")
-# options.add_argument("--width=1200")
-# service = Service(executable_path=f'{current_dir}/geckodriver', log_path=os.devnull)
-# driver = webdriver.Firefox(options=options, service=service)
 service = Service(executable_path=f'{current_dir}/chromedriver', log_path=os.devnull)
 driver = webdriver.Chrome(options=options, service=service)
 driver.get(f'file:///{current_dir}/web/index.html')
 
 labelError = False
 
-for label in modules.foliera.parse(filepath):
+if company == "f":
+    renderer = modules.foliera.parse(filepath)
+if company == "t":
+    renderer = modules.trillium.parse(filepath)
+    
+for label in renderer:
     print(f"rendering label: {label}")
     try:
         render_label(company=label.get('company', ''),
